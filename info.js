18cbe8235dde4f08b604eb534952dfbe5c657718
Oscar <oheredia2@ucol.mx>
2022-07-12 21:55:56 -0500

    agregue info

---
+++ info.js
@@ -0,0 +1,66 @@
+/*--   TIPOS DE DATOS  --*/
+//Cadenas Tipo de dato string
+var nombre = "Carlos";
+console.log(nombre);
+
+//Tipo de dato númerico 
+var numero = 100;
+
+
+//Tipo de dato objeto 
+
+var objeto = {
+    nombre : "juan",
+    apellido : "Perez",
+    telefono : "2253"
+}
+
+//Type of (Nombre de la variable)
+
+//Tipo de dato boolean (true o false)
+var bandera = true;
+
+//Tipo de dato function 
+function miFuncion()
+{
+}
+console.log(miFuncion);
+
+//Tipo de dato Symbol
+var  simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//Tipo clase es una function 
+class persona 
+{
+    constructor(nombre,apellido)
+    {
+        this.nombre=nombre;
+        this.apellido=apellido;
+
+    }
+}
+
+console.log(persona);
+
+//Tipo de dato undefined
+
+var x;
+console.log(x);
+
+//tipo de dato null = ausencia de valor;
+
+var y = null;
+console.log(y);
+
+//arrays - arreglos
+var autos = ["BMW","AUDI","Volvo"];
+console.log(autos);
+
+//Concatenar cadenas
+
+var Nombre = "Juan";
+var Apellidos = "Perez";
+
+
+
