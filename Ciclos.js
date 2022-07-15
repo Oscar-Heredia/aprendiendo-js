429376318609dfd3320940fac358fb3cd25fa389
Oscar <oheredia2@ucol.mx>
2022-07-14 21:44:38 -0500

    Arreglos

--- Ciclos.js
+++ Ciclos.js
@@ -7,3 +7,20 @@ while(contador < 3)
     console.log(contador);
     contador++;
 }
+
+//do - while
+
+do{
+    contador.log(contador);
+    contador++;
+}while(contador<3);
+
+//for
+
+for(let contador = 0; contador<10; contador++)
+{   
+    if(contador % 2 == 0)
+    {
+        console.log(contador);
+    }
+}
